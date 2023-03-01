94ff48f6883c94c293df11c3f94869663e518fa8
Daniel Szemerey <account@szemerey.eu>
2023-03-01 12:17:18 +0100

    feature: Added dummy table with link.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // import Plot from 'react-plotly.js';
-import { DataFrame, fromCSV, readFile, Series } from 'data-forge';
+import { DataFrame, fromCSV, Series } from 'data-forge';
+import { close } from 'fs';
 import dynamic from 'next/dynamic';
 
 import { Meta } from '@/layouts/Meta';
@@ -7,10 +8,39 @@ import { Main } from '@/templates/Main';
 import OneSection from '@/templates/OneSection';
 
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
+const Table = (props) => {
+  const values = [
+    ['Books', 'Clothes', 'Medicals'],
+    [
+      '$22',
+      '$190',
+      '<a href="https://github.com/dream-faster/benchmarking-test">Link to site</a>',
+    ],
+  ];
+  const headers = [['<b> Item </b>'], ['<b> Expenditure </b>']];
+  const data = [
+    {
+      type: 'table',
+      header: {
+        values: headers,
+        align: 'center',
+        presentation: 'markdown',
+      },
+      cells: {
+        values,
+        align: 'center',
+      },
+    },
+  ];
+
+  return (
+    <Plot data={data} layout={{ width: 500, height: 500, title: 'Table' }} />
+  );
+};
 
 export default function Index({
-  resultsData,
-  df,
+  date,
+  close,
 }: {
   allPostsData: [string, string, string, string];
   allTopicsData: [string, string, string, string];
@@ -32,16 +62,16 @@ export default function Index({
         <Plot
           data={[
             {
-              x: df.getSeries('Close').toArray(),
-              y: [allPostsData * 2, allPostsData * 3, allPostsData * 4],
+              x: date,
+              y: close,
               type: 'scatter',
               mode: 'lines+markers',
               marker: { color: 'red' },
             },
-            { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
           ]}
           layout={{ title: 'A Fancy Plot' }}
         />
+        <Table />
       </OneSection>
 
       {/* <OneSection>
@@ -75,28 +105,44 @@ export default function Index({
   );
 }
 
+function fetchCsv() {
+  return fetch(
+    'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/bitcoin.csv'
+  ).then(function (response) {
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder('utf-8');
+
+    return reader.read().then(function (result) {
+      return decoder.decode(result.value);
+    });
+  });
+}
+
 export async function getStaticProps() {
   // const allPostsData = getSortedPostsData();
   // const allTopicsData = getSortedTopicsData();
 
-  const res = await fetch(
-    'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/model.csv'
-  );
-  const resultsData = await res.json();
-  const bitcoin = await fetch(
-    'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/bitcoin.csv'
-  );
-  const bitcoinData = await bitcoin.json();
-
+  // const res = await fetch(
+  //   'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/model.csv'
+  // );
+  // const resultsData = await res.json();
+  // const bitcoin = await fetch(
+  //   'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/bitcoin.csv'
+  // );
+  const bitcoinData = await fetchCsv();
+  console.log(bitcoinData);
   // raw.githubusercontent.com/Unsigned-Research/enoki-research/main/results.csv?token=GHSAT0AAAAAAB65Q4QLH63AHK64XZSFPI2CY77HANA
 
   // const allTopicsData = allPostsData;
+
   const df = fromCSV(bitcoinData);
+  const close = df.getSeries('Close').toArray();
+  const date = df.getSeries('Date').toArray();
 
   return {
     props: {
-      resultsData,
-      df,
+      date,
+      close,
     },
   };
 }
